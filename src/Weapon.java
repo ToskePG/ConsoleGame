2c41dd3a29c2fe583a3c08f4d7aefacb8505d6be
Danilo Tosic <54001767+ToskePG@users.noreply.github.com>
2024-12-29 13:03:11 +0100

    Adds contructors for weapon.

--- src/Weapon.java
+++ src/Weapon.java
@@ -1,4 +1,27 @@
+import java.util.*;
 
 public class Weapon {
 
+	private String name;
+	private int damage;
+	private double weight;
+	
+	
+	// Default Constructor
+	public Weapon()
+	{
+		this.name = "Deafult player";
+		this.damage = 1;
+		this.weight = 10.0;
+	}
+	
+	// Create a Weapon
+	public Weapon(String name, int damage, double weight)
+	{
+		this.name = name;
+		this.damage = damage;
+		this.weight = weight;
+	}
+	
+	
 }
